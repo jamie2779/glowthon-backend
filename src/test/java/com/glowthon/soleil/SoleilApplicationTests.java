589399f14bf318d91f11a04c1232e9a71918a417
jamie2779 <jamie2779@naver.com>
2024-05-29 11:16:23 +0900

    feat: Base Spring Code

---
+++ src/test/java/com/glowthon/soleil/SoleilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glowthon.soleil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoleilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
